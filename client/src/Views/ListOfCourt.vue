9058c452a99067d44720e34face1bdfb626a4cad
pthanhtung412 <75255316+pthanhtung412@users.noreply.github.com>
2023-12-17 14:34:27 +0700

    update list of Court

--- client/src/Views/ListOfCourt.vue
+++ client/src/Views/ListOfCourt.vue
@@ -1,52 +1,64 @@
 <template>
     <div>
         <div class="container">
-            <div class="item1">
+            <div class="tieuDe">
+                <h1>DANH SÁCH SÂN CẦU LÔNG</h1>
+            </div>
+            <div v-for="(item, index) in items" :key="index" :class="{ 'item1': index % 2 === 0, 'item2': index % 2 !== 0 }">
                 <div class="left-part">
                     <div class="image-container">
-                        <img src="../assets/images/badminton court/1696400900372-co-nen-kinh-doanh-san-cau-long.jpg">
+                        <img :src= "item.imageSrc" alt="CourtBadminton">
                     </div>
                 </div>
                 <div class="right-part">
-                    <div class="court-name">Sân cầu lông Thiên Vân</div>
-                    <p>
-                        <i class=" fas fa-home text-white" style="font-size: 23px; margin-right: 5px;"></i>
-                        57 Nguyễn Nghiêm, Phú Trung, Tân Phú, Thành phố Hồ Chí Minh
-                    </p>
-                    <p>
-                        <i class=" fas fa-phone text-white" style="font-size: 23px; margin-right: 5px;"></i>
-                        0913404924
-                    </p>
+                    <div class="court-name">{{ item.courtName }}</div>
+                        <p>
+                            <i class="fas fa-home text-white" style="font-size: 23px; margin-right: 5px;"></i>
+                            {{ item.address }}
+                        </p>
+                        <p>
+                            <i class="fas fa-phone text-white" style="font-size: 23px; margin-right: 5px;"></i>
+                            {{ item.phoneNumber }}
+                        </p>
                 </div>
-            </div>
-            <div class="item2">
-                fsddfsdfdsssssssssssssssssssssss
-            </div>
-            <div class="item1">
-                fsddfsdfdsssssssssssssssssssssss
-            </div>
-            <div class="item2">
-                fsddfsdfdsssssssssssssssssssssss
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  data() {
+    return {
+      items: [
+        { imageSrc: "https://sonsanepoxy.vn/wp-content/uploads/2023/07/lap-dat-he-thong-den-chieu-san-cau-long.jpg", courtName: "Sân cầu lông Thiên Vân", address: "57 Nguyễn Nghiêm, Phú Trung, Tân Phú, Thành phố Hồ Chí Minh", phoneNumber: "0913 404 924" },
+        { imageSrc: "https://limosa.vn/wp-content/uploads/2023/08/san-cau-long-cay-keo.jpg", courtName: "Sân cầu lông ABC", address: "710/53/2, Lũy Bán Bích, Tân Thành, Tân Phú, Thành phố Hồ Chí Minh", phoneNumber: "0983 916 646" },
+        { imageSrc: "https://sieuthicaulong.vn/images/badminton-yard/1688728199_gallery_san-cau-long-tan-phuc-1.jpg", courtName: "Sân cầu lông Tấn Phúc", address: "36/48 Huỳnh Thiện Lộc, Hoà Thanh, Tân Phú, Thành phố Hồ Chí Minh", phoneNumber: "0903 938 919" },
+        { imageSrc: "https://badmintonw.com/uploads/images/gioi-thieu-san-cau-long-tao-dan-diem-den-li-tuong-cho-long-thu-quan-1-10.png", courtName: "Sân cầu lông Viettel", address: "57 Nguyễn Nghiêm, Phú Trung, Tân Phú, Thành phố Hồ Chí Minh", phoneNumber: "0913404924" },
+        { imageSrc: "https://sieuthicaulong.vn/images/badminton-yard/1688381528_gallery_22.PNG", courtName: "Sân cầu lông Lam Sơn", address: "320/1 Đ. Trần Bình Trọng, Phường 4, Quận 5, Thành phố Hồ Chí Minh", phoneNumber: "0909 222 958" },
+        // Add more items as needed
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .container {
     width: 100%;
     height: auto;
-    padding-top: 200px;
+    padding-top: 160px;
     padding-bottom: 100px;
     display: flex;
     flex-direction: column;
     align-items: center; /* Căn giữa theo chiều ngang */
+}
+
+.tieuDe { //schua chữ danh sách cầu lông
+    display: block;
+    padding-top: 2px;
+    padding-bottom: 60px;
+    color: #ffffff;
 }
 
 .item1,
