565500a3ca6411500fa44959a462aaacf164bcea
fefefafa01 <khoa.trinhkaiz@hcmut.edu.vn>
2024-06-02 18:36:09 +0700

    change something

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/BCB/",
+  // base: "/BCB/",
   plugins: [vue()],
   resolve: {
     alias: {
